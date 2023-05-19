9cccfc098ba3d7815a74ba39dcfc59bb26d0005f
Rohit511 <rathorerohit511@gmail.com>
2023-05-19 09:13:42 +0530

    mes12

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,34 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  let [start,setStart]=useState(false);
+  function hi(){
+    setStart(true);
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={hi}>click me</button>
+      {start?<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy</p>:null}
     </div>
   );
+
+  //  let [start,setStart]=useState(false);
+
+	// function hi(){
+	// 	setStart(true);
+	// }
+  //   	return(
+  //   		<div id="main">
+	// 			{ /* Do not remove this main div!! */ }
+	// 			<button id="click" onClick={hi} >click me</button>
+	// 			{start?<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes
+  //         me so happy</p>:null}
+				
+  //   		</div>
+  //   	);
+    
+
 }
 
 
